4a1c8a3218645c7531795859fa1ac28e9a567985
Joao-Gabriel-SB <joao_gabriel_bispo@hotmail.com>
2025-07-09 20:03:21 +0000

    esqueci meu nome nos arquivos

--- Trabalho 2/EstruturaVetores.h
+++ Trabalho 2/EstruturaVetores.h
@@ -1,6 +1,27 @@
 #ifndef TRABALHO2_ESTRUTURAVETORES_H
 #define TRABALHO2_ESTRUTURAVETORES_H
+// #################################################
+//  Instituto Federal da Bahia
+//  Salvador - BA
+//  Curso de Análise e Desenvolvimento de Sistemas http://ads.ifba.edu.br
+//  Disciplina: INF029 - Laboratório de Programação
+//  Professor: Renato Novais - renato@ifba.edu.br
 
+//  ----- Orientações gerais -----
+//  Descrição: esse arquivo deve conter as questões do trabalho do aluno.
+//  Cada aluno deve renomear esse arquivo para Aluno<MATRICULA>.c
+//  O aluno deve preencher seus dados abaixo, e implementar as questões do trabalho
+
+//  ----- Dados do Aluno -----
+//  Nome: João Gabriel de Santana Bispo
+//  email: joao_gabriel_bispo@hotmail.com
+//  Matrícula:20242160011@ifba.edu.br
+//  Semestre:2025.1
+
+//  Copyright © 2016 Renato Novais. All rights reserved.
+// Última atualização: 07/05/2021 - 19/08/2016
+
+// #################################################
 // enumeracoes (enum) ajudam a deixar o codigo mais legivel, possibilitando que voce de significado
 // as suas constantes, para mais informacoes https://en.cppreference.com/w/c/language/enum
 // as constantes do enum em sua inicialização vao representar o numero resultante da soma do valor da
